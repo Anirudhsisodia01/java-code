632409c1f7a19b7440d90b6d97735e8b98549563
anirudh <anirudhrajput.ind2345@gmail.com>
2024-11-06 23:36:03 +0530

    first commit

---
+++ scanner/scanner.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class scanner 
+{
+    public static void main(String[] args)
+     {
+        Scanner sc = new Scanner(System.in);
+
+        int a= sc.nextInt();
+        System.out.println("a"+a);
+
+        int b= sc.nextInt();
+        System.out.println("b"+b);
+
+        int c= sc.nextInt();
+        System.out.println("c"+c);
+    }
+}
