44cafe61c3c93c53caaec02a6d8694be1f18e284
anirudh <anirudhrajput.ind2345@gmail.com>
2024-11-07 00:37:26 +0530

    scanner/scanner.class

--- scanner/scanner.java
+++ scanner/scanner.java
@@ -5,6 +5,7 @@ public class scanner
     public static void main(String[] args)
      {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a Integer");
 
         int a= sc.nextInt();
         System.out.println("a"+a);
